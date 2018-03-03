ec1217577ee03d8a548c23f70918a540a38eb842
Anonymous Raccoon <32224410+AnonymusRaccoon@users.noreply.github.com>
2018-03-03 22:59:40 +0100

    Adding unity project

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update ()
+    {
+        rb.AddForce(new Vector3(Input.GetAxis("Horizontal") , 0, 0));
+	}
+}
