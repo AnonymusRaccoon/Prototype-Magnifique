8d4f4c27e65851b8ae1a0bc65bc6e55d3b5cb57c
Anonymous Raccoon <32224410+AnonymusRaccoon@users.noreply.github.com>
2018-03-05 14:59:23 +0100

    Starting wall jump and moving plateform implementation

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -8,13 +8,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int jumpForce = 8;
     [SerializeField] private int smallJump = 4;
     [SerializeField] private float gravity = 8.5f;
+    [SerializeField] private int wallJump = 200;
+    [SerializeField] private int wallJumpPush = 250;
 
     [Space]
     [SerializeField] private LayerMask groundMask;
     [SerializeField] private LayerMask wallMask;
 
     private bool isGrounded = true;
+    private bool canCancel = false;
     private bool canWallJump = false;
+    private int wallDirection;
     private float jumpDirection;
     private bool airControl = false;
 
@@ -29,14 +33,26 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         if (Physics.OverlapSphere(rb.position, 1.5f, groundMask).Length > 0)
+        {
             isGrounded = true;
+            canCancel = false;
+        }
         else
             isGrounded = false;
 
-        if (Physics.OverlapSphere(rb.position, 1, wallMask).Length > 0)
+        Collider[] walls = Physics.OverlapSphere(rb.position, 1, wallMask);
+        if (walls.Length > 0)
+        {
             canWallJump = true;
+            if(walls[0].transform.position.x > rb.position.x)
+                wallDirection = -1;
+            else
+                wallDirection = 1;
+        }
         else
+        {
             canWallJump = false;
+        }
 
 
         if (!isGrounded)
@@ -57,7 +73,7 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        rb.MovePosition(rb.position + new Vector3(Input.GetAxis("Horizontal"), 0, 0) * (airControl ? airSpeed : speed) * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + new Vector3(Input.GetAxis("Horizontal") * (airControl ? airSpeed : speed) * Time.fixedDeltaTime - rb.velocity.x, 0, 0));
         airControl = false;
 
 
@@ -65,17 +81,29 @@ public class PlayerMovement : MonoBehaviour
         {
             jumpDirection = Input.GetAxis("Horizontal");
             isGrounded = false;
-            rb.velocity = new Vector3(0, jumpForce, 0);
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+            canCancel = true;
         }
-        if(Input.GetButtonUp("Jump") && !isGrounded && rb.velocity.y > smallJump)
+        if (Input.GetButtonUp("Jump") && !isGrounded && rb.velocity.y > smallJump && canCancel)
         {
-            rb.velocity = new Vector3(0, smallJump, 0);
+            canCancel = false;
+            rb.velocity = new Vector3(rb.velocity.x, smallJump, rb.velocity.z);
         }
 
 
         if (rb.velocity.y < 1)
         {
-            rb.velocity = new Vector3(0, rb.velocity.y - gravity * Time.fixedDeltaTime, 0);
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - gravity * Time.fixedDeltaTime, rb.velocity.z);
+        }
+
+
+        if (canWallJump && !isGrounded && Input.GetButtonDown("Jump"))
+        {
+            print("Wall Jumping");
+            canCancel = false;
+            jumpDirection = wallDirection;
+            rb.velocity = new Vector3(wallJumpPush * -wallDirection, wallJump, rb.velocity.z);
+            //rb.AddForce(new Vector3(wallJumpPush * -wallDirection, wallJump, 0), ForceMode.Acceleration);
         }
     }
 }
